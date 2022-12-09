5f71472d5b2fb7e672cdb0545dfb682c3740d7e2
root <ben.sbanotto@gmail.com>
2022-12-09 14:55:42 +0000

    Had event listener for api status in checkbox lool

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const amenities = [];
   const amenitiesName = [];
   $('li :checkbox').change(function() {
-    if (this.checked) {
-      amenities.push($(this).attr('data-id'));
-      amenitiesName.push($(this).attr('data-name'));
-    } else {
-      amenities.splice($.inArray($(this).attr('data-id'), amenities), 1);
-      amenitiesName.splice($.inArray($(this).attr('data-name'), amenitiesName), 1);
-    }
-    $('.amenities h4').html(amenitiesName.join(', '));
+  if (this.checked) {
+    amenities.push($(this).attr('data-id'));
+    amenitiesName.push($(this).attr('data-name'));
+  } else {
+    amenities.splice($.inArray($(this).attr('data-id'), amenities), 1);
+    amenitiesName.splice($.inArray($(this).attr('data-name'), amenitiesName), 1);
+  }
+  $('.amenities h4').html(amenitiesName.join(', '));
   });
   const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data) {
